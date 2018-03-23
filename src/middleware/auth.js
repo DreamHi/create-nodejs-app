656b8692d50ea1885b9faf417d7c35e158c3c991
sl-say <sl_say2016@qq.com>
2018-03-23 19:13:23 +0900

    修改token头定义

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,11 +1,12 @@
 const async       = require("async");
 const createError = require("http-errors");
 const response    = require("../core/response");
+const config      = require("../../config/app");
 const ctrlToken   = require("../modules/system/controllers/ctrl_token");
 
 exports.authenticate = (req, res, next) => {
 
-  const token  = req.headers["x-token-hione"] || "";
+  const token  = req.headers[config.tokenHeader] || "";
 
   if (!token) {
     response.send(req, res, new createError.Unauthorized(), "");
